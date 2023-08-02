89ed6c8f86b93d9346de47f4b8ced57caf1243e5
= <=>
2023-08-02 07:54:11 +0100

    r

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import IconShop from "../components/icons/shop";
 import Animator from "../components/Animator";
 const GetStripePrices=async ()=>{
   
-  const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET ?? '',{
+  const stripe = new Stripe('sk_test_51NJVyhDt6cUT5aTWuiobYjYMmg9RhpFdJ4b8MhPMuOs2ahr3T41eoR4Q6h8x9506DDpLyK2U89gyHiQ2cxCY5FzT00CFlik7Hj' ?? '',{
       apiVersion:'2022-11-15'
   })
   const prices = await stripe.prices.list({
@@ -19,7 +19,7 @@ const GetStripePrices=async ()=>{
         
       
     },{
-      apiKey:process.env.NEXT_PUBLIC_STRIPE_SECRET
+      apiKey:'sk_test_51NJVyhDt6cUT5aTWuiobYjYMmg9RhpFdJ4b8MhPMuOs2ahr3T41eoR4Q6h8x9506DDpLyK2U89gyHiQ2cxCY5FzT00CFlik7Hj'
     });
   console.log(prices.data[0],"") 
   if (prices.data.length===0){
