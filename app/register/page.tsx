550376e3503e913aa3fb70dad96037f76be11538
= <=>
2023-08-08 11:49:10 +0100

    r

---
+++ app/register/page.tsx
@@ -0,0 +1,22 @@
+
+
+import React from 'react'
+
+import Link from 'next/link'
+import { MdClose } from 'react-icons/md'
+import { RegisterForm } from '../../components/Auth/RegisterForm'
+
+const RegisterPage = () => {
+    
+  return (
+    <main className='mt-0 flex flex-col 
+    items-center justify-center'>
+      <Link 
+      className='absolute top-4 left-4 text-white'
+      href="/"><MdClose/></Link>
+          <RegisterForm/>
+    </main>
+  )
+}
+
+export default RegisterPage
---
+++ components/Auth/RegisterForm.tsx
@@ -0,0 +1,46 @@
+"use client"
+import Link from 'next/link'
+import React, { useState } from 'react'
+
+export const RegisterForm = () => {
+    const [email,setEmail]=useState("")
+    const [pw,setPw]=useState("")
+    const [pw2,setPw2]=useState("")
+
+    const handleLogin = (e)=>{
+      e.preventDefault()
+    }
+  return (
+    <form 
+    onSubmit={handleLogin}
+    className='auth__form'>
+      <h1>Register</h1>
+        <label 
+        className=''
+        htmlFor='email'>Email</label>
+        <input 
+        className=''
+        name='email' id='email'
+        type='email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+        <label htmlFor='pw'>Password</label>
+        <input 
+        name='pw' id='pw'
+        type='password' value={pw} onChange={(e)=>setPw(e.target.value)}/>
+         <label htmlFor='pw2'>Confirm Password</label>
+        <input 
+        name='pw2' id='pw2'
+        type='password' value={pw2} onChange={(e)=>setPw2(e.target.value)}/>
+      <button
+      disabled={pw.length===0||email.length===0 ||pw2.length===0}
+      className=''
+      >Register</button>
+      <Link
+      href="/"
+      >Forgot Password</Link>
+
+      <Link href="/register">
+      Have an account already? Sign In
+      </Link>
+    </form>
+  )
+}
