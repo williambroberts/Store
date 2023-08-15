920ce704c997561786eaf91ccd05c994cda76414
= <=>
2023-08-15 20:06:00 +0100

    r

--- app/contact/ContactForm.tsx
+++ app/contact/ContactForm.tsx
@@ -4,12 +4,13 @@ export const ContactForm = () => {
   return (
     <section className='contact__form__container'>
         <h3>Message Us</h3>
-        <form action="" className='contact__form'>
-            <div className='grid grid-cols-1 md:grid-cols-2'>
+        <form action={`https://formsubmit.co/williambrobertsemail@gmail.com`} className='contact__form' method='POST'>
+            <div className='grid grid-cols-1 md:grid-cols-2 w-full'>
                 <input type="text" name='name' placeholder='name' required id="name"/>
                 <input type="email" name='email' placeholder='email' required id="email"/>
             </div>
-            <textarea name="ta" id="ta" cols="30" rows="10" required placeholder='Message'></textarea>
+            <textarea name="ta" id="ta" cols={30} rows={10} required placeholder='Message'></textarea>
+            <button>Send</button>
         </form>
     </section>
   )
---
+++ app/contact/ContactHelp.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const ContactHelp = () => {
+  return (
+    <section>
+        <h3>Help</h3>
+        
+    </section>
+  )
+}
