1fe1cc468bb897a0a9e3d0f687f7268f18861bbf
= <=>
2023-08-15 11:39:30 +0100

    more added

--- app/about/Components/About.tsx
+++ app/about/Components/About.tsx
@@ -5,8 +5,10 @@ export const About = () => {
     <article className='about__container'>
         <h2 className='subheading'>Meet us</h2>
         <section className='flex flex-col gap-4 text-[var(--t-1)]'>
-            <h3 className='text-xl font-semibold'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam, ratione!</h3>
-            <p className='text-sm text-[var(--t-3)]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi saepe nulla ad ducimus totam deserunt dolor, dolorum quo possimus sequi!</p>
+            <h3 className='text-xl font-semibold'>About us, our company and our manifesto!</h3>
+            <p className='text-sm text-[var(--t-3)]'>We aim to sell the highest quality mockups to you. Our products are hand selected with close attention and absolute care 
+            <strong className='px-0.5'><em>Every time!</em></strong>
+            </p>
         </section>
     </article>
   )
--- app/about/Components/ReachOut.tsx
+++ app/about/Components/ReachOut.tsx
@@ -1,11 +1,31 @@
+import Link from 'next/link'
 import React from 'react'
+import { BsArrowRight } from 'react-icons/bs';
 
 export const ReachOut = () => {
   return (
     <article className='about__container'>
         <h2 className='subheading'>More</h2>
         <section className='flex flex-col gap-4'>
+          <MoreLink href='/' heading='View our products'/>
+          <MoreLink href='/' heading='See our FAQ'/>
         </section>
     </article>
+  )
+}
+
+
+interface linkProps {
+  href?:string;
+  heading?:string;
+}
+const MoreLink = ({href,heading}:linkProps)=>{
+  return (
+    <Link 
+    className='more__link'
+    href={href}>
+      {heading}
+<BsArrowRight/>
+    </Link>
   )
 }
