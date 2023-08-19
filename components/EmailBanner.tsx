22ed54fb4690b2134fd0bb6dec761863c4c38aa2
= <=>
2023-08-19 11:05:12 +0100

    r

--- components/EmailBanner.tsx
+++ components/EmailBanner.tsx
@@ -1,7 +1,11 @@
 "use client"
 import React, { useState } from 'react'
-
+import {v4}from "uuid"
 export const EmailBanner = () => {
+  let nums = Array(60).fill(1)
+  nums = nums.map(i=>{
+    return Math.floor(Math.random() * 100)/10
+  })
     const [email,setEmail]=useState("")
     const handleClick = (e)=>{
       e.preventDefault()
@@ -10,7 +14,17 @@ export const EmailBanner = () => {
     <div 
     aria-label='email'
     className='email__banner'>
-         <div className='flex 
+       <div className="lines">
+          {nums.map((item,index)=> <div 
+         
+          className="line" key={v4()}>
+            <div 
+            style={{animationDelay:`${item}s`}}
+            className="line__after"></div>
+          </div>)}
+         
+        </div>
+         <div className='flex  z-10
          max-w-[650px] items-center
          flex-col w-full p-6 gap-0'>
             <h2>Subscribe &amp; benefit right away!</h2>
