0d0bbdd9bacc4c92587c104c35ede684b3effe9a
= <=>
2023-09-15 20:02:55 +0100

    r

--- utils/base.ts
+++ utils/base.ts
@@ -1,7 +1,8 @@
 const base  = "https://cookie-api-pbcn.onrender.com"
+const base2="https://erin-earthworm-cuff.cyclic.cloud"
 export default base
 const local = `http://localhost:5000`
 
-const origin =base
+const origin =base2
 
 export {local,origin}
